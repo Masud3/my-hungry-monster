c0bf8eb54b1ffdf752246fb0fdb4191ae2c5a548
Masud3 <mdamdadulm79@gmail.com>
2021-02-08 22:34:49 -0800

    added style

--- app.js
+++ app.js
@@ -11,10 +11,34 @@ const displayMeals = meals => {
     const mealContainer = document.getElementById('meal-container');
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
-        mealDiv.className = 'single-result row align-items-center text-center my-3 p-3';
-        mealDiv.innerHTML = `
-            <img src="${meal.strMealThumb}" class="rounded float-start" alt="...">
-        `;
+        mealDiv.className = 'meal';
+        const mealInfo = `
+        <img src="${meal.strMealThumb}" class="img" alt="...">
+        <h3 class="h3">${meal.strMeal}</h3>
+        `
+        mealDiv.innerHTML = mealInfo;
+
         mealContainer.appendChild(mealDiv);
-    })
+    });
 }
+
+
+
+
+
+
+
+
+// const displayMeals = meals => {
+//     const mealContainer = document.getElementById('meal-container');
+//     meals.forEach(meal => {
+//         const mealDiv = document.createElement('div');
+//         mealDiv.className = 'meal';
+//         mealDiv.className = 'single-result row align-items-center text-center my-3 p-3';
+//         mealDiv.innerHTML = `
+//             <img src="${meal.strMealThumb}" class="rounded float-start" alt="...">
+//             <h3>${meal.strMeal}</h3>
+//         `;
+//         mealContainer.appendChild(mealDiv);
+//     })
+// }
