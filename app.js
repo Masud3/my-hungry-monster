2bbddb2b92937664eb1ad6ab3bcd3de4dc3b2db8
Masud3 <mdamdadulm79@gmail.com>
2021-06-29 10:16:25 -0800

    add all everything

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+/************  Data load of meal *********** */
 const searchMeal = () =>{
     const searchText = document.getElementById('search-place').value;
-    const url =`https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`
-    // load data
+    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals))
 }
 
+const mealContainer = document.getElementById('meal-container');
 const displayMeals = meals => {
-    const mealContainer = document.getElementById('meal-container');
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
         mealDiv.className = 'meal';
         const mealInfo = `
         <img src="${meal.strMealThumb}" class="img" alt="...">
         <h3 class="h3">${meal.strMeal}</h3>
-        <button class="detailsBtn" onclick="aboutMealDetail('${meal.meal}')">Details</button>
+        <button class="detailsBtn" onclick="aboutMealDetail('${meal.strMeal}')">Details</button>
         `
         mealDiv.innerHTML = mealInfo;
 
@@ -23,16 +23,28 @@ const displayMeals = meals => {
     });
 }
 
+    /********** meal details information starts   ************ */
+
+
 const aboutMealDetail = meal => {
-    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood/meal/${meal}`;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`
     fetch(url)
     .then(res => res.json())
-    .then(data => renderMealInfo(meal[0]));
+    .then(data => renderMealInfo(data.meals[0]));
 }
+
 
 const renderMealInfo = meal => {
     const mealDiv = document.getElementById("mealDetail");
     mealDiv.innerHTML = `
-    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}"/>
+    <h1 class="detail-name">Name: ${meal.strMeal}</h1>
+    <h5>Area: ${meal.strArea}</h5>
+    <p>${meal.strCategory}</p>
+    <p><small>${meal.strIngredient4}</small></p>
+    <p><small>${meal.strIngredient5}</small></p>
+    <p><small>${meal.strIngredient6}</small></p>
+    <p><small>${meal.strIngredient7}</small></p>
+    <p><small>${meal.strIngredient8}</small></p>
     `
 }
